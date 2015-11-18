29386cc975d46f81cee93c7a031d5e8790e9db62
Pablite5 <Pablite5@GAMING>
2015-11-18 10:55:21 +0100

    54

---
+++ src/bol9/pkg5/Bol95.java
@@ -0,0 +1,16 @@
+
+package bol9.pkg5;
+
+/**
+ *
+ * @author Pablite5
+ */
+public class Bol95 {
+
+    
+    public static void main(String[] args) {
+        Calcular obx =new Calcular ();
+        obx.calcularSeries();
+    }
+    
+}
